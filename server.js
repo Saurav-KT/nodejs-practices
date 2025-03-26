797d7c0f114ee23a7765ab2cd5248d3daf381e81
saurav j <saurav.saurav12@gmail.com>
2025-03-26 23:22:40 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import http from 'http';
+const PORT= 8000;
+const server= http.createServer((req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.end('<h1>Hello World ! </h1>')
+});
+
+server.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
